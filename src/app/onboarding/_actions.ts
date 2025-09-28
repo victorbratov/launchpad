47863cec4cb4df7d6f8184b9182ef2661aaeafca
victorbratov <victor@bratov.com>
2025-09-28 23:49:04 +0100

    wip

--- src/app/onboarding/_actions.ts
+++ src/app/onboarding/_actions.ts
@@ -3,6 +3,7 @@
 import { db } from '@/db';
 import { BusinessAccount, InvestorAccounts } from '@/db/schema';
 import { auth, clerkClient, currentUser } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
 
 export const completeOnboarding = async (role: string, name: string, bank_account_number: string) => {
   const { isAuthenticated, userId } = await auth();
@@ -18,34 +19,27 @@ export const completeOnboarding = async (role: string, name: string, bank_accoun
     return { success: true };
   }
 
-  try {
-    if (role === 'investor') {
-      await db.insert(InvestorAccounts).values({
-        InvestorID: userId,
-        InvEmail: clerkUser?.emailAddresses[0]?.emailAddress || '',
-        InvName: name,
-        InvBankACNumber: bank_account_number,
-        InvWallet: '0.00'
-      });
-    } else if (role === 'business') {
-      await db.insert(BusinessAccount).values({
-        BusAccountID: userId,
-        BusEmail: clerkUser?.emailAddresses[0]?.emailAddress || '',
-        BusName: name,
-        BusBankAcc: bank_account_number,
-        BusWallet: '0.00'
-      });
-    }
-
-    const res = await client.users.updateUser(userId, {
-      publicMetadata: { onboardingComplete: true },
-    })
-
-    console.log('Onboarding complete:', res);
-
-    return { success: true }
-
-  } catch (error) {
-    return { success: false }
+  if (role === 'investor') {
+    await db.insert(InvestorAccounts).values({
+      InvestorID: userId,
+      InvEmail: clerkUser?.emailAddresses[0]?.emailAddress || '',
+      InvName: name,
+      InvBankACNumber: bank_account_number,
+      InvWallet: '0.00'
+    });
+  } else if (role === 'business') {
+    await db.insert(BusinessAccount).values({
+      BusAccountID: userId,
+      BusEmail: clerkUser?.emailAddresses[0]?.emailAddress || '',
+      BusName: name,
+      BusBankAcc: bank_account_number,
+      BusWallet: '0.00'
+    });
   }
+
+  await client.users.updateUser(userId, {
+    publicMetadata: { onboardingComplete: true },
+  })
+
+  redirect("/")
 }
