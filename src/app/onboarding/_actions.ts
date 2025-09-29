a29f039533742bc5313a8085bb7c28b68568e66e
laurac42 <114315963+laurac42@users.noreply.github.com>
2025-09-29 18:19:02 +0100

    Fixing business owner sign in

--- src/app/onboarding/_actions.ts
+++ src/app/onboarding/_actions.ts
@@ -1,7 +1,7 @@
 'use server'
 
 import { db } from '@/db';
-import { BusinessAccount, InvestorAccounts } from '@/db/schema';
+import { BusinessAccount, InvestorAccounts, TheBank } from '@/db/schema';
 import { auth, clerkClient, currentUser } from '@clerk/nextjs/server';
 import { redirect } from 'next/navigation';
 
@@ -28,13 +28,25 @@ export const completeOnboarding = async (role: string, name: string, bank_accoun
       InvWallet: '0.00'
     });
   } else if (role === 'business') {
-    await db.insert(BusinessAccount).values({
-      BusAccountID: userId,
-      BusEmail: clerkUser?.emailAddresses[0]?.emailAddress || '',
-      BusName: name,
-      BusBankAcc: bank_account_number,
-      BusWallet: '0.00'
-    });
+    try {
+      // store the businesses bank account in the bank table as this is needed to create the account
+      await db.insert(TheBank).values({
+        BankAccountNumber: bank_account_number,
+        Ballance: '0.00'
+      });
+
+      await db.insert(BusinessAccount).values({
+        BusAccountID: userId,
+        BusEmail: clerkUser?.emailAddresses[0]?.emailAddress || '',
+        BusName: name,
+        BusBankAcc: bank_account_number,
+        BusWallet: '0.00'
+      });
+    } catch (error) {
+      console.error("Error inserting business account: ", error);
+      return { success: false, message: 'Error creating business account' };
+    }
+
   }
 
   await client.users.updateUser(userId, {
