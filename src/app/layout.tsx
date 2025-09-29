41692ce1fb6af16c6c22b42ee48e19c55a914d8e
victorbratov <victor@bratov.com>
2025-09-29 07:38:31 +0100

    wip

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import { ClerkProvider } from '@clerk/nextjs';
 import "./globals.css";
 import { MantineProvider } from '@mantine/core';
-import Navbar from "@/components/navbar";
+import Navbar from "@/components/Navbar";
 import { jetBrainsMono } from "./font";
 
 export const metadata: Metadata = {
