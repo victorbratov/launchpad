59f598b77200daff87ee4f4ba18474c9a4b1efee
laurac42 <114315963+laurac42@users.noreply.github.com>
2025-10-03 18:10:31 +0100

    Withdraw from business account works

--- src/app/business-portal/_actions.ts
+++ src/app/business-portal/_actions.ts
@@ -1,13 +1,14 @@
 "use server"
 
 import { db } from '@/db';
-import { BusinessAccount } from '@/db/schema';
+import { BusinessAccount, TheBank } from '@/db/schema';
 import { auth } from '@clerk/nextjs/server';
-import { eq } from 'drizzle-orm';
+import { eq, sql } from 'drizzle-orm';
 
 /**
  * Get the information about the business account of the current user
- * @returns 
+ * @returns Business account information including name, email and wallet balance
+ * @throws Error if the user is not authenticated
  */
 export const getBusinessAccountInfo = async () => {
     const { isAuthenticated, userId } = await auth();
@@ -18,3 +19,24 @@ export const getBusinessAccountInfo = async () => {
     const businessAccount = await db.select({name: BusinessAccount.BusName, email: BusinessAccount.BusEmail, wallet: BusinessAccount.BusWallet} ).from(BusinessAccount).where(eq(BusinessAccount.BusAccountID, userId)).limit(1);
     return businessAccount[0];
 };
+
+/**
+ * Withdraw from the account balance of the current user into their linked bank account
+ * @param balance The amount to be withdrawn
+ * @throws Error if the user is not authenticated
+ */
+export const withdrawBalance = async (balance: number) => {
+    const { isAuthenticated, userId } = await auth();
+    if (!isAuthenticated) {
+        throw new Error('User not authenticated');
+    }
+    // update the business account wallet
+    const [updatedAccount] = await db.update(BusinessAccount).set({
+         BusWallet: sql`${BusinessAccount.BusWallet} - ${balance}`
+    }).where(eq(BusinessAccount.BusAccountID, userId)).returning();;
+
+    // update the bank account 
+    await db.update(TheBank).set({
+        Ballance: sql`${TheBank.Ballance} + ${balance}`
+    }).where(eq(TheBank.BankAccountNumber, updatedAccount.BusBankAcc)); 
+}
